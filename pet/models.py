01feab594f573655ae3b12061ca4b162466d9b05
Murillo Augusto <murilloaugusto@vxcaseloja.com>
2022-05-18 21:51:29 -0300

    feat: make models and migrations/migrate

--- pet/models.py
+++ pet/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Group(models.Model):
+    name = models.CharField(max_length=255)
+    scientific_name = models.CharField(max_length=255)
+
+class Animal(models.Model):
+    name = models.CharField(max_length=255)
+    age = models.FloatField()
+    weight = models.FloatField()
+    sex = models.CharField(max_length=255)
+    group = models.ForeignKey(Group, on_delete=models.DO_NOTHING)
+
+class Characteristc(models.Model):
+    name = models.CharField(max_length=255)
+    animals = models.ManyToManyField(Animal)
